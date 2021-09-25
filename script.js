119023f81eca5bdbfc78e859f0d19f2e899b7b86
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-09-25 11:45:38 -0300

    ajuste nome

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ function user(){
 
     const name = document.querySelector("#name").value;
     const p = document.querySelector("#user");
-        if(name.textContent == "" ) {
+
+        if(name == "" ) {
             alert("Digite seu nome.");
         } else {
-            p.textContent = `Olá ${name}.`;            
+            p.textContent = `Olá ${name}.`;  
+            var sec = document.querySelector("#projetos");
+                  
+            sec.style.display = "flex"; //mudando o style do item          
         };
     
-        var sec = document.querySelector("#projetos");
         
-           
-        sec.style.display = "flex"; //mudando o style do item
             
         
         
