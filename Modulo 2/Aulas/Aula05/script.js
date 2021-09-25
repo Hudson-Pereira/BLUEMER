e2735fb15d62850d6444e77051de86b4b0393945
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-09-24 23:36:14 -0300

    lista e projeto 1 quase pronto

---
+++ Modulo 2/Aulas/Aula05/script.js
@@ -0,0 +1,49 @@
+// window.onload = alert('Oi');
+function sec1(){
+    var sec = document.querySelector("#IMC");
+
+    if (sec.style.display === "none") {
+        sec.style.display = "flex";
+    } else {
+        sec.style.display = "none"
+    };
+};
+
+function sec2(){
+    var sec = document.querySelector("#Listas");
+
+    if (sec.style.display === "none") { // verificando o style do item
+        sec.style.display = "flex"; //mudando o style do item
+    } else {
+        sec.style.display = "none"
+    };
+};
+
+function dado(){
+    const name = document.querySelector("#nome").value;
+    const age = document.querySelector("#idade").value;
+    const data = document.querySelector("#dados");
+    const peso = document.querySelector("#peso").value;
+    const alt = document.querySelector("#altura").value;
+
+    if (name == "" || age == "" || peso == "" || alt == ""){
+        alert("Preencha todos os campos corretamente.");
+    } else {
+    const res = peso / (alt * alt); 
+
+    data.textContent = `Oi ${name}, que tem ${age} anos. Seu IMC é ${res.toFixed(2)}.`; //modificando o texto do item
+    };
+};
+
+    const btn = document.querySelector("#btn");
+
+    btn.addEventListener("click", function(){
+    const li = document.createElement("li"); //criando li da lista
+    const tarefa = document.querySelector("#task").value;
+
+    const taskText = document.createTextNode(tarefa); //criando o texto na variavel
+    li.appendChild(taskText); //add o texto na lista
+
+    document.querySelector("#tasks").appendChild(li); // add a lista na ul
+    document.querySelector("#task").value = ""; //zerando o campo
+});
--- Modulo 2/Aulas/Projeto1/script.js
+++ Modulo 2/Aulas/Projeto1/script.js
@@ -5,37 +5,37 @@ const pers = document.querySelector("#pers");
 // window.onload = alert(nPers);
 Btn.addEventListener("click", function(){
 
-    if (nPers.innerText == "Sonic") {
-    nPers.innerText = "Tails";
+    if (nPers.textContent == "Sonic") {
+    nPers.textContent = "Tails";
     pers.src = "../Projeto1/images/Tails-min.png";
-    } else if (nPers.innerText == "Tails") {
-        nPers.innerText = "Rouge";
+    } else if (nPers.textContent == "Tails") {
+        nPers.textContent = "Rouge";
         pers.src = "../Projeto1/images/Rouge.png"; 
-    } else if (nPers.innerText == "Rouge"){
-        nPers.innerText = "Knuckles";
+    } else if (nPers.textContent == "Rouge"){
+        nPers.textContent = "Knuckles";
         pers.src = "../Projeto1/images/knuckles.png";
-    } else if (nPers.innerText == "Knuckles"){
-        nPers.innerText = "Amy Rose";
+    } else if (nPers.textContent == "Knuckles"){
+        nPers.textContent = "Amy Rose";
         pers.src = "../Projeto1/images/Amy_Rose.png";
-    } else if (nPers.innerText == "Amy Rose"){
-        nPers.innerText = "Sticks";
+    } else if (nPers.textContent == "Amy Rose"){
+        nPers.textContent = "Sticks";
         pers.src = "../Projeto1/images/Sticks.png";
-    } else if (nPers.innerText == "Sticks"){
-        nPers.innerText = "Shadow";
+    } else if (nPers.textContent == "Sticks"){
+        nPers.textContent = "Shadow";
         pers.src = "../Projeto1/images/Shadow.png";
-    } else if (nPers.innerText == "Shadow"){
-        nPers.innerText = "Unleashed";
+    } else if (nPers.textContent == "Shadow"){
+        nPers.textContent = "Unleashed";
         pers.src = "../Projeto1/images/unleashed.png";
-    } else if (nPers.innerText == "Unleashed"){
-        nPers.innerText = "Turma";
+    } else if (nPers.textContent == "Unleashed"){
+        nPers.textContent = "Turma";
         pers.src = "../Projeto1/images/turma.png";
     }else {
-        nPers.innerText = "Sonic";
+        nPers.textContent = "Sonic";
         pers.src = "../Projeto1/images/Sonic.png";
     };
 });
 
 // Btn.addEventListener("click", function() {
-//     nPers.innerText = "Rouge";
+//     nPers.textContent = "Rouge";
 //     rouge.src = "../Projeto1/images/Rouge.png";
 // });
