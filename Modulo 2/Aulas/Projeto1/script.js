2acf6f6c289cc9495a8fb548d0da08ed3d73ab06
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-09-24 21:33:44 -0300

    mudando nome pasta

--- Modulo 2/Aulas/Projeto1/script.js
+++ Modulo 2/Aulas/Projeto1/script.js
@@ -7,35 +7,35 @@ Btn.addEventListener("click", function(){
 
     if (nPers.innerText == "Sonic") {
     nPers.innerText = "Tails";
-    pers.src = "../Aula02-projeto1/images/Tails-min.png";
+    pers.src = "../Projeto1/images/Tails-min.png";
     } else if (nPers.innerText == "Tails") {
         nPers.innerText = "Rouge";
-        pers.src = "../Aula02-projeto1/images/Rouge.png"; 
+        pers.src = "../Projeto1/images/Rouge.png"; 
     } else if (nPers.innerText == "Rouge"){
         nPers.innerText = "Knuckles";
-        pers.src = "../Aula02-projeto1/images/knuckles.png";
+        pers.src = "../Projeto1/images/knuckles.png";
     } else if (nPers.innerText == "Knuckles"){
         nPers.innerText = "Amy Rose";
-        pers.src = "../Aula02-projeto1/images/Amy_Rose.png";
+        pers.src = "../Projeto1/images/Amy_Rose.png";
     } else if (nPers.innerText == "Amy Rose"){
         nPers.innerText = "Sticks";
-        pers.src = "../Aula02-projeto1/images/Sticks.png";
+        pers.src = "../Projeto1/images/Sticks.png";
     } else if (nPers.innerText == "Sticks"){
         nPers.innerText = "Shadow";
-        pers.src = "../Aula02-projeto1/images/Shadow.png";
+        pers.src = "../Projeto1/images/Shadow.png";
     } else if (nPers.innerText == "Shadow"){
         nPers.innerText = "Unleashed";
-        pers.src = "../Aula02-projeto1/images/unleashed.png";
+        pers.src = "../Projeto1/images/unleashed.png";
     } else if (nPers.innerText == "Unleashed"){
         nPers.innerText = "Turma";
-        pers.src = "../Aula02-projeto1/images/turma.png";
+        pers.src = "../Projeto1/images/turma.png";
     }else {
         nPers.innerText = "Sonic";
-        pers.src = "../Aula02-projeto1/images/Sonic.png";
+        pers.src = "../Projeto1/images/Sonic.png";
     };
 });
 
 // Btn.addEventListener("click", function() {
 //     nPers.innerText = "Rouge";
-//     rouge.src = "../Aula02-projeto1/images/Rouge.png";
+//     rouge.src = "../Projeto1/images/Rouge.png";
 // });
