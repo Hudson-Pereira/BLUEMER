7ceb9febb7c526bd2f14fdd3ba55d261594ed950
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-09-24 21:03:30 -0300

    andamento projeto1

--- Modulo 2/Aulas/Aula02-projeto1/script.js
+++ Modulo 2/Aulas/Aula02-projeto1/script.js
@@ -1,14 +1,41 @@
 const Btn = document.querySelector("#btn");
 const nPers = document.querySelector("#nPers");
-const tails = document.querySelector("#pers");
-const rouge = document.querySelector("#pers")
+const pers = document.querySelector("#pers");
+// var rouge = document.querySelector("#pers");
+// window.onload = alert(nPers);
+Btn.addEventListener("click", function(){
 
-Btn.addEventListener("click", function() {
+    if (nPers.innerText == "Sonic") {
     nPers.innerText = "Tails";
-    tails.src = "../Aula02-projeto1/images/Tails.png";
+    pers.src = "../Aula02-projeto1/images/Tails-min.png";
+    } else if (nPers.innerText == "Tails") {
+        nPers.innerText = "Rouge";
+        pers.src = "../Aula02-projeto1/images/Rouge.png"; 
+    } else if (nPers.innerText == "Rouge"){
+        nPers.innerText = "Knuckles";
+        pers.src = "../Aula02-projeto1/images/knuckles.png";
+    } else if (nPers.innerText == "Knuckles"){
+        nPers.innerText = "Amy Rose";
+        pers.src = "../Aula02-projeto1/images/Amy_Rose.png";
+    } else if (nPers.innerText == "Amy Rose"){
+        nPers.innerText = "Sticks";
+        pers.src = "../Aula02-projeto1/images/Sticks.png";
+    } else if (nPers.innerText == "Sticks"){
+        nPers.innerText = "Shadow";
+        pers.src = "../Aula02-projeto1/images/Shadow.png";
+    } else if (nPers.innerText == "Shadow"){
+        nPers.innerText = "Unleashed";
+        pers.src = "../Aula02-projeto1/images/unleashed.png";
+    } else if (nPers.innerText == "Unleashed"){
+        nPers.innerText = "Turma";
+        pers.src = "../Aula02-projeto1/images/turma.png";
+    }else {
+        nPers.innerText = "Sonic";
+        pers.src = "../Aula02-projeto1/images/Sonic.png";
+    };
 });
 
-Btn.addEventListener("click", function() {
-    nPers.innerText = "Rouge";
-    rouge.src = "../Aula02-projeto1/images/Rouge.png";
-});
+// Btn.addEventListener("click", function() {
+//     nPers.innerText = "Rouge";
+//     rouge.src = "../Aula02-projeto1/images/Rouge.png";
+// });
