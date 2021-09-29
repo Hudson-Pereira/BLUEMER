76b0787ccd92663b7a27825fc000fd969d02d533
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-09-29 20:29:10 -0300

    form

--- Modulo_2/Aulas/Projeto2/index.js
+++ Modulo_2/Aulas/Projeto2/index.js
@@ -1,14 +1,15 @@
 const express = require("express");//importa modulo express
-const path = require("path");
+const path = require("path"); //importando path
 
 const app = express();//instancia uma referencia do express no projeto
 const port = 3000;
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs"); // set engine para trabalhar com EJS
+app.use(express.static(path.join(__dirname, "public"))); //set public como raiz
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.send("Hello world uhuuuu");
 });
 
 app.get("/index", (req, res) => {
@@ -21,12 +22,24 @@ app.get("/test", (req, res) => {
 });
 
 app.get("/cadastro", (req, res) => {
-    res.render("cadastro");
+    res.render("cadastro");    
 });
 
-app.get("/details", (req, res) => {
-    const conteudos = [{img: "/img/charmander.jpg", num:"001", name: "Charmander",type: "Fogo",desc: "Descricao", alt: "0.6",peso: "8.5", hab:"Chamas"}]
-    res.render("details", { pokedados: conteudos });
+app.post("/details", (req, res) => {
+    // const conteudos = [{img: "/img/charmander.jpg", num:"001", name: "Charmander",type: "Fogo",desc: "Descricao", alt: "0.6",peso: "8.5", hab:"Chamas"}]
+    const {imagem, name, num, type, hab, alt, peso} = req.body;
+
+    res.send({imagem: imagem, name: name, num: num, type: type, hab: hab, alt: alt, peso: peso});
+});
+
+app.get("/formulario", (req, res) => {
+    res.render("formulario");
+});
+
+app.post("/recebeform", (req,res) => {
+    const {nome, email, senha} = req.body;
+
+    res.send({nome: nome, email: email, senha: senha});
 });
 
 
