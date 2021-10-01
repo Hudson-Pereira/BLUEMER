364802f85ceb4bfbaa84ce5974e8149b022b591e
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-09-30 23:08:18 -0300

    ajustes pokedex

--- Modulo_2/Aulas/Projeto2/index.js
+++ Modulo_2/Aulas/Projeto2/index.js
@@ -8,15 +8,17 @@ app.set("view engine", "ejs"); // set engine para trabalhar com EJS
 app.use(express.static(path.join(__dirname, "public"))); //set public como raiz
 app.use(express.urlencoded());
 
+//variaveis
+let pokemons = [{img: "/img/charmander.jpg", num:"001", name: "Charmander",type: "Fogo",desc: "Descricao", alt: "0.6",peso: "8.5", hab:"Chamas"}];
+let message = "";
+// app.get("/", (req, res) => {
+//     res.send("Hello world uhuuuu");
+// });
 
-var pokemons = [];
 app.get("/", (req, res) => {
-    res.send("Hello world uhuuuu");
-});
-
-app.get("/index", (req, res) => {
-    const conteudos = ["Nome","Tipo","Altura","Peso"]
-    res.render("index", { titulo: "Pokédex", conteudos: conteudos });
+    
+    const poke = pokemons;
+    res.render("index", {poke: poke});
 });
 
 app.get("/test", (req, res) => {
@@ -27,34 +29,43 @@ app.get("/cadastro", (req, res) => {
     res.render("cadastro");    
 });
 
-app.get("/details", (req, res) => {
-    const conteudos = [{img: "/img/charmander.jpg", num:"001", name: "Charmander",type: "Fogo",desc: "Descricao", alt: "0.6",peso: "8.5", hab:"Chamas"}]
-    res.render("details", { pokedados: conteudos});
+app.get("/detail", (req, res) => {
+    const conteudos = {img: "/img/charmander.jpg", num:"001", name: "Charmander",type: "Fogo",desc: "Descricao", alt: "0.6",peso: "8.5", hab:"Chamas"}
+    
+    const props = Object.keys(conteudos);
+    
+    res.render("detail", { pokedados: conteudos, props: props});
 });
 
 app.post("/details", (req, res) => {
     
     const {imagem, name, num, type, desc, hab, alt, peso} = req.body;
 
-    const pokedados = [{img: imagem, name: name, num: num, type: type, desc: desc, hab: hab, alt: alt, peso: peso}];
+    const pokedados = {img: imagem, name: name, num: num, type: type, desc: desc, hab: hab, alt: alt, peso: peso};
+    
     pokemons.push(pokedados);
+
+    const props = Object.keys(pokedados);
+    
     console.log(pokemons);
-    res.render("details", {pokedados: pokedados, todos: pokemons});
+    res.render("details", {pokemons: pokemons});
 });
 
 app.get("/formulario", (req, res) => {
-    res.render("formulario");
+        setTimeout(() => {
+        message = "";
+    }, 2000);
+
+    res.render("formulario", {message});
 });
 
 app.post("/recebeform", (req,res) => {
     const {nome, email, senha} = req.body; // recuperando via http post
-    const infor = {nome: nome, email: email, senha: senha, message: `Usuário ${nome} cadastrado com sucesso.`};
-//construindo JSON com informacoes
-    setTimeout(() => {
-        infor.message = ""
-    },1000);
-
-    res.render("recebeform", infor); //renderizando e usando o JSON para exibir na tela
+    message = `Usuário ${nome} cadastrado com sucesso. Um e-mail de confirmação foi enviado para ${email}.`;
+     
+   
+    res.redirect("/formulario");   
+    // res.render("recebeform", infor); //renderizando e usando o JSON para exibir na tela
 });
 
 
--- Modulo_2/Aulas/Projeto2/public/js/script.js
+++ Modulo_2/Aulas/Projeto2/public/js/script.js
@@ -1,6 +1,59 @@
-const closeMessage = document.querySelector("#close");
-const message = document.querySelector("#message");
+const left = document.querySelector("#left");
+const pokemon = document.querySelector("#pokemon");
+const pok = document.querySelector("#name");
+const right = document.querySelector("#right");
+const tipo = document.querySelector("#tipo");
+const evo1 = document.querySelector("#evo1");
 
-closeMessage.addEventListener("click", function(){
-    message.style.display = "none"
+left.addEventListener("click", function() {
+    if (pok.textContent == "Charmander") {
+        pokemon.src = "img/squirtle.jpg";
+        pok.textContent = "Squirtle";
+        tipo.textContent = "Agua";
+    } else if (pok.textContent == "Squirtle") {
+    //     pokemon.src = "img/charizard.jpg";
+    //     pok.textContent = "Charizard";
+    // } else if (pok.textContent == "Charizard") {
+        pokemon.src = "img/bulba.jpg";
+        pok.textContent = "Bulbasauro";
+        tipo.textContent = "Planta";
+    } else {
+    pokemon.src = "img/charmander.jpg";
+    pok.textContent = "Charmander";
+    tipo.textContent = "Fogo";
+    }
+});
+
+right.addEventListener("click", function() {
+    if (pok.textContent == "Charmander") {
+        pokemon.src = "img/bulba.jpg";
+        pok.textContent = "Bulbassauro";
+        tipo.textContent = "Planta";
+    } else if (pok.textContent == "Bulbassauro") {
+    //     pokemon.src = "img/charmilion.jpg";
+    //     pok.textContent = "Charmeleon";
+        pokemon.src = "img/squirtle.jpg";
+        pok.textContent = "Squirtle";
+        tipo.textContent = "Agua";
+    } else {
+        pokemon.src = "img/charmander.jpg";
+        pok.textContent = "Charmander";
+        tipo.textContent = "Fogo";
+    }
+});
+
+evo1.addEventListener("click", function(){
+    if (pok.textContent == "Charmander") {
+        pokemon.src = "img/charmilion-2.jpg";
+        pok.textContent = "Charmeleon";
+        tipo.textContent = "Fogo";
+    } else if (pok.textContent == "Bulbassauro") {
+        pokemon.src = "img/ivi.jpeg";
+        pok.textContent = "Ivysauro";
+        tipo.textContent = "Planta";
+    } else {
+        pokemon.src = "img/wartotle.jpg";
+        pok.textContent = "Wartotle";
+        tipo.textContent = "Agua";
+    }  
 });
